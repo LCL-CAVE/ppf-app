8fe0371f114a19b9bd46c9e9cdf80dfbe5ad9434
Mohammad Namakshenas <m.n.shenas@gmail.com>
2023-10-31 16:20:22 +0100

    removed redundant imports

--- layouts/ly_output_layout.py
+++ layouts/ly_output_layout.py
@@ -1,6 +1,4 @@
 from dash import html
-import dash_mantine_components as dmc
-from dash_iconify import DashIconify as dicon
 from components.c_kpi_group import create_kpi_group
 from components.c_display_chart_group import create_display_chart_group
 from controls.cl_button_output_group import create_btn_output_group
