abec5ac15cea1f16f34789bc87aacfbae6d72a15
Mohammad Namakshenas <m.n.shenas@gmail.com>
2023-11-02 09:17:31 +0100

    id added to time_horizon.json and a new atomic python for date_picker

---
+++ components/c_date_picker.py
@@ -0,0 +1,37 @@
+import dash_mantine_components as dmc
+from dash import html
+import time
+from datetime import datetime, timedelta, date
+
+
+# item_list : .json
+# context: string e.g., "demand"
+def create_date_picker(item, context):
+    return dmc.DateRangePicker(
+        label=item["label"],
+        id="date_picker_" + context + "_" + item["id"],
+        mb=15,
+        fullWidth=True,
+        value=[
+            date(
+                item['initial_year'],
+                item['initial_month'],
+                item['initial_day'],
+            ),
+            date(
+                item['initial_year'],
+                item['initial_month'],
+                item['initial_day'],
+            ) + timedelta(days=365)
+        ],
+        minDate=date(
+            item['min_year'],
+            item['min_month'],
+            item['min_day'],
+        ),
+        maxDate=date(
+            item['max_year'],
+            item['max_month'],
+            item['max_day'],
+        ),
+    )
--- components/c_date_picker_group.py
+++ components/c_date_picker_group.py
@@ -1,47 +1,13 @@
-import dash_mantine_components as dmc
 from dash import html
-import time
-from datetime import datetime, timedelta, date
+from components.c_date_picker import create_date_picker
 
 
 # item_list : .json
 # context: string e.g., "demand"
 def create_date_picker_group(item_list, context):
-    return dmc.Group(
+    return html.Div(
         [
-            html.Div(
-                [
-                    dmc.DateRangePicker(
-                        label=item["label"],
-                        id="date_picker_" + context + "_" + item["label"].lower(),
-                        value=[
-                            date(
-                                item['initial_year'],
-                                item['initial_month'],
-                                item['initial_day'],
-                            ),
-                            date(
-                                item['initial_year'],
-                                item['initial_month'],
-                                item['initial_day'],
-                            ) + timedelta(days=365)
-                        ],
-                        minDate=date(
-                            item['min_year'],
-                            item['min_month'],
-                            item['min_day'],
-                        ),
-                        maxDate=date(
-                            item['max_year'],
-                            item['max_month'],
-                            item['max_day'],
-                        ),
-                        mb=10,
-                    ),
-                ],
-                style={'width': "100%"},
-            )
+            create_date_picker(item, context)
             for item in item_list
         ],
-        mb=15
     )
