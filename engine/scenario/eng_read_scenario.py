ac1e6d2e5f9d694499da2b3daea5052c1563cf79
Mohammad Namakshenas <m.n.shenas@gmail.com>
2024-02-29 15:50:01 +0100

    Fix minor bug

--- engine/scenario/eng_read_scenario.py
+++ engine/scenario/eng_read_scenario.py
@@ -5,6 +5,7 @@ import pickle
 import os
 from engine.scenario.eng_scenario_generate import serve_eng_generate_scenarios
 
+
 def serve_read_scenario(initial_capacity,
                         growth_rate,
                         scenario_start_date,
@@ -16,13 +17,12 @@ def serve_read_scenario(initial_capacity,
     # scenario_start_date = "2024-06-01 00:00"
     # scenario_end_date = "2028-06-01 00:00"
 
-
     config = {
         "num_scenarios": 10,  # Number of solar generation scenarios to generate
         "scenario_start_date": scenario_start_date,  # The start date for creating scenarios
         "scenario_end_date": scenario_end_date,  # User-defined end date for scenarios
-        "initial_capacity": initial_capacity*1000,
-        "growth_rate": growth_rate/100,
+        "initial_capacity": initial_capacity * 1000,
+        "growth_rate": growth_rate / 100,
         "cluster_count": 4,  # Number of clusters for the errors and creating a transition matrix
         "transition_frequency": "2W",  # Frequency of transition (e.g., every 2 weeks)
     }
